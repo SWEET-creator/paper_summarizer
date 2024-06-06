e619c9ca6a309d7482f81d4925bef66831868410
SWEET-creator <sosui0131@icloud.com>
2024-06-06 16:44:50 +0900

    optimize code

--- add_notion.py
+++ add_notion.py
@@ -12,8 +12,14 @@ notion = Client(auth=config.NOTION_API_KEY)
 # データベースIDを設定
 database_id = config.database_id
 
+# カラム情報を設定
+columns = config.columns
+
 def add_summary2notion(pdf_path):
-    plain_text = get_summary(pdf_path)
+    #plain_text = get_summary(pdf_path)
+    plain_text = """
+                    { "Name": "High-Resolution Image Synthesis with Latent Diffusion Models", "どんなもの？": "この研究は、効率的で高品質な高解像度画像生成を目指して、潜在空間に基づく拡散モデル（LDM）を提案しています。", "先行研究と比較して新規性は？": "従来の拡散モデルがピクセル空間で動作していたのに対し、本研究は高解像度での画像生成を行うために、潜在空間で拡散モデルを動作させる点が新規です。", "手法のキモは？": "まず、有力な事前訓練されたオートエンコーダを使用して、画像を低次元の潜在空間に圧縮します。その後、この潜在空間で拡散モデルを訓練します。また、クロスアテンション層を導入し、一般的な条件付け入力を受け付ける柔軟なモデルを構築します。", "有効性はどのように検証された？": "提案したLDMは、画像補完、クラス条件付き画像生成、テキスト条件付き画像生成、超解像など様々なタスクで最先端の手法と比較して高い性能を示しました。また、従来のモデルと比べて計算効率が大幅に向上しました。", "課題と議論は？": "生成した画像の質は高いが、GANのような速度はまだ達成されていません。また、潜在空間の再構成能力は、ピクセルレベルの精度を要求されるタスクには制約があります。", "次に読む論文等は？": "分類器フリー拡散誘導、他の条件付き生成タスクへの応用、GANと拡散モデルの融合に関する研究。", "Keywords": ["Latent Diffusion Models", "High-Resolution Image Synthesis", "Denoising Autoencoders", "Cross-Attention"] }
+                """
     
     if plain_text == None:
         return
@@ -31,78 +37,52 @@ def add_summary2notion(pdf_path):
         
     keywords = [{"name" : keyword} for keyword in json_data['Keywords']]
 
-    # 新しいページを追加するデータ
     new_page_data = {
         "parent": {"database_id": database_id},
-        "properties": {
-            "Name": {
+        "properties": {},
+        "children": [
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": str(json_data)
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+
+    for column in columns:
+        if column == "Keywords":
+            new_page_data["properties"][column] = {
+                "multi_select": keywords
+            }
+        elif column == "Name":
+            new_page_data["properties"][column] = {
                 "title": [
                     {
                         "text": {
-                            "content": json_data['Name']
+                            "content": json_data[column]
                         }
                     }
                 ]
-            },
-            "どんなもの？": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": json_data['どんなもの？']
-                        }
-                    }
-                ]
-            },
-            "先行研究と比較して新規性は？": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": json_data['先行研究と比較して新規性は？']
-                        }
-                    }
-                ]
-            },
-            "手法のキモは？": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": json_data['手法のキモは？']
-                        }
-                    }
-                ]
-            },
-            "有効性はどのように検証された？": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": json_data['有効性はどのように検証された？']
-                        }
-                    }
-                ]
-            },
-            "課題と議論は？": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": json_data['課題と議論は？']
-                        }
-                    }
-                ]
-            },
-            "次に読む論文等は？": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": str(json_data['次に読む論文等は？'])
-                        }
-                    }
-                ]
-            },
-            "Keywords": {
-                "multi_select": keywords
             }
-        }
-    }
+        else:
+            new_page_data["properties"][column] = {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": json_data[column]
+                        }
+                    }
+                ]
+            }
 
     # Notionのデータベースに新しいページを追加
     response = notion.pages.create(**new_page_data)
@@ -112,7 +92,7 @@ def add_summary2notion(pdf_path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download and summarize arXiv paper")
-    parser.add_argument("pdf_path", type=str, help="The pdf path want to make summarize")
+    parser.add_argument("pdf_path", nargs='?', default= "downloaded-paper.pdf", type=str, help="The pdf path want to make summarize")
     args = parser.parse_args()
     
     add_summary2notion(args.pdf_path)
