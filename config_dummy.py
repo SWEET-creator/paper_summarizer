7bb8e008e75f6697a827e303acc9f8d02ed3f6ec
SWEET-creator <sosui0131@icloud.com>
2024-06-06 16:44:19 +0900

    add columns

--- config_dummy.py
+++ config_dummy.py
@@ -1,3 +1,14 @@
 OPENAI_API_KEY = ''# Enter your open ai api
 NOTION_API_KEY = ''# Enter your notion api secrets
 database_id = ''# Enter your database id
+
+columns = [
+        "Name",
+        "どんなもの？",
+        "先行研究と比較して新規性は？",
+        "手法のキモは？",
+        "有効性はどのように検証された？",
+        "課題と議論は？",
+        "次に読む論文等は？",
+        "Keywords"
+    ]
